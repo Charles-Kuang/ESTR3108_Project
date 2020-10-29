ffb6c1976fad18f9ba8f6679cc7e031c904b88c3
Charles-Kuang <1155124427@link.cuhk.edu.hk>
2020-10-29 15:47:49 +0800

    Test [ ]

---
+++ TIMELINE.md
@@ -0,0 +1,12 @@
+10.27
+Learn Pytorch mainly from https://pytorch.org/get-started/locally/
+
+10.28
+Learn DRN from https://www.cnblogs.com/wzyuan/p/9880342.html
+Unsolved: 
+        [] why 'labels' in Cifar10 is of size 4 instead of size 10
+
+10.29
+Unsolved: 
+        [] what can model_zoo & model_urls do?
+        [] what can 'expansion' do? (why is 4 but not 5, 6)
--- code/ResNet.py
+++ code/ResNet.py
@@ -1,1 +1,23 @@
 import torch
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo#
+
+__all__ = ['ResNet', 'resnet18']
+
+model_urls = {
+     'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+}#
+
+#define 3x3 convolutional layer
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+#define residual block
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, outplanes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, outplanes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(outplanes)
