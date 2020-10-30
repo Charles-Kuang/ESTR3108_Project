f7f615f8066da3ff87d0ec39c8139560a7cb7d82
CHEN Tianhao <54804476+patrickChenTH@users.noreply.github.com>
2020-10-30 15:03:21 +0800

    dataset loaders added

---
+++ code/dataset_3.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+import torch
+import torch.utils.data
+from skimage import io
+from torchvision import transforms
+
+train_img_path = '../data/task3/training_data'
+train_gt_path = '../data/task3/training_gt.csv'
+test_img_path = '../data/task3/test_data'
+test_gt_path = '../data/task3/test_gt.csv'
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+class LoadDataset3(torch.utils.data.Dataset):
+    train_img_path = '../data/task3/training_data'
+    train_gt_path = '../data/task3/training_gt.csv'
+    test_img_path = '../data/task3/test_data'
+    test_gt_path = '../data/task3/test_gt.csv'
+    train_list = pd.read_csv(train_gt_path, header=None).values.tolist()
+    test_list = pd.read_csv(test_gt_path, header=None).values.tolist()
+
+    def __init__(self, transform=None, train=True):
+        self.transform = transform
+        self.train = train
+
+    def __len__(self):
+        if self.train:
+            return len(self.train_list)
+        else:
+            return len(self.test_list)
+
+    def __getitem__(self, idx):
+        if self.train:
+            img_name = self.train_list[idx][0] + '.jpg'
+            image, label = io.imread(os.path.join(train_img_path, img_name)), self.train_list[idx][1]
+            image = transform(image)
+        else:
+            img_name = self.test_list[idx][0] + '.jpg'
+            image, label = io.imread(os.path.join(test_img_path, img_name)), self.test_list[idx][1]
+            image = transform(image)
+        return image, label
